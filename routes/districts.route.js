322cf1f56e59181acdb0574eb20203a4f2de5518
vijaysariyam <65538789+vijaysariyam@users.noreply.github.com>
2023-12-02 16:36:00 +0530

    byid

--- routes/districts.route.js
+++ routes/districts.route.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const controller = require("../controllers/districts.controller.js");
+const express = require('express');
+const controller = require('../controllers/districts.controller.js');
 const router = express.Router();
-router.get("/districts/getall", controller.getAll);
-router.get("/districts/:id", controller.getById);
-router.post("/districts/", controller.create);
-router.put("/districts/:id", controller.updateById);
-router.delete("/districts/:id", controller.deletedById);
+router.get('/districts/getall', controller.getAll);
+router.get('/districts/:id', controller.getById);
+router.post('/districts/', controller.create);
+router.put('/districts/:id', controller.updateById);
+router.delete('/districts/:id', controller.deletedById);
 
 //join queries
 router.get('/getalldistrictswithjoin', controller.getAllWithJoin);
 
+router.get('/getdistrictsbystateid/:id', controller.getDisctrictsByStateId);
 
 module.exports = router;
