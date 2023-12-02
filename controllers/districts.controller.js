5f76c7116e8bdb0bb41d08a16a41559eaff6d245
Naga sai <129513089+naga-sai1@users.noreply.github.com>
2023-12-02 10:12:33 +0530

    add updated API in districts
    Add updated API in districts

--- controllers/districts.controller.js
+++ controllers/districts.controller.js
@@ -48,8 +48,8 @@ async function updateById(req, res) {
       const data = await Districts.findByPk(req.params.id);
       if (!data) throw new HTTPError(404, `id: ${req.params.id} was not found`);
        // Update district properties
-      if (req.body.title) data.title = req.body.title;
-      if (req.body.description) data.description = req.body.description;
+      if (req.body.state_id) data.state_id = req.body.state_id;
+      if (req.body.district_name) data.district_name = req.body.district_name;
       await data.save();
       return res.status(200).json({ message: data });
     } catch (e) {
